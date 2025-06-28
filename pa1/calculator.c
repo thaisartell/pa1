9c8931c76a8d71db7372749fa61815cf3b81214a
Kip D Schetnan <schet076@csel-remote-lnx-01.cselabs.umn.edu>
2025-06-28 14:38:28 -0500

    Added code to calculator.c

--- pa1/calculator.c
+++ pa1/calculator.c
@@ -1,4 +1,8 @@
 #include "calculator.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Function prototypes */
 /* Feel free to add your own helper functions */
@@ -12,7 +16,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     
-    const char *dir_path;
+    const char *dir_path = argv[1];
     /* TODO: Get the path to the directory from command line options */ 
 
     /* TODO: Process the directory */
@@ -23,14 +27,17 @@ int main(int argc, char *argv[]) {
 
 void process_directory(const char *dir_path) {
     /* TODO: Task 1 - Directory handling */
-        
+    DIR *dir = opendir(dir_path);
     /* Open directory */
         
     /* Count files first to allocate memory */
     int num_files = count_files_in_directory(dir_path);
-        
+    if (num_files == 0) {
+        closedir(dir);
+        return; // No files to process
+    }
     /* TODO: Allocate memory for results */
-        
+    child_pids = malloc(num_files * sizeof(pid_t));
     /* Phase 2: Create pipes for communication */
     /* TODO: Task 4 - Create pipes here */
     
@@ -39,7 +46,7 @@ void process_directory(const char *dir_path) {
     /* TODO: Task 2 - Wait for all child processes to complete */
     
     /* TODO: Close directory here */
-    
+    closedir(dir);
     /* Calculate and print final results */
    
     /* TODO: Remember to deallocate any memory */
@@ -47,6 +54,23 @@ void process_directory(const char *dir_path) {
 
 int count_files_in_directory(const char *dir_path) {
     int count = 0;
+
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir_path, entry->d_name);
+
+        struct stat path_stat;
+        if (stat(full_path, &path_stat) == 0 && S_ISREG(path_stat.st_mode)) {
+            count++;
+        }
+    }
+
+    closedir(dir);
 
     return count;
 }
