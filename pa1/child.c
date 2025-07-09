f62ba98c19267211aeaa8f6fc3c8bb60b1ebbd78
thaisartell <thaisartell@gmail.com>
2025-07-09 18:14:05 -0500

    Pre-pipe task 4 changes

--- pa1/child.c
+++ pa1/child.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     
     /* Phase 2: Both file path and pipe fd are needed */
     if (argc < 2 || argc > 3) {
-        fprintf(stderr, "Usage: %s FILE_PATH [PIPE_FD]\n", argv[0]);
+        fprintf(stderr, "Usage: %s FILE_PATH [PIPE_FD]\n", argv[0]); 
         exit(1);
     }
    
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]) {
     file_path = argv[1];
 
     /* TODO: Get the file descriptor from the command line in Phase 2 */
+    pipe_fd = atoi(argv[2]);
 
     /* Process the file */
     process_file(file_path, pipe_fd);
@@ -53,8 +54,11 @@ void process_file(const char *file_path, int pipe_fd){
     write_results_to_file(file_path, count, sum);
     
     /* Phase 2: Send results via pipe */
+
     /* TODO: Task 4 - Implement pipe communication */
-    /* send_results_via_pipe(pipe_fd, count, sum); */
+    if (pipe_fd > -1) {
+        send_results_via_pipe(pipe_fd, count, sum);
+    }
 }
 
 void write_results_to_file(const char *original_path, int count, long sum) {
@@ -74,4 +78,8 @@ void send_results_via_pipe(int pipe_fd, int count, long sum){
     /* This function will be implemented in Phase 2 */
     /* TODO: Write message to pipe */
     /* Hint: The pipe file descriptor should be passed somehow */
+    
+    char* msg;
+    write(pipe_fd, msg, sizeof(msg));
+
 }
